98294a50858c77cf761472ecb9b75ea909d14875
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-09-10 22:50:12 -0300

    Concluindo parte gráfica do projeto

--- projeto_gasolina_etanol/src/App.tsx
+++ projeto_gasolina_etanol/src/App.tsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
+import logoImg from './assets/logo.png'
+
 function App() {
   return (
       <div>
-        <h1>TESTE</h1>
+        <main className="container">
+            <img
+            className="logo"
+            src={logoImg} 
+            alt="Logo da calculadora de gasolina ou etanol"/>
+            <h1 className="title">Qual a melhor opção?</h1>
+
+            <form className="form">
+              <label>Álcool (preço por litro):</label>
+              <input 
+              className="input"
+              type="number"
+              placeholder="4,90"
+              min="1"
+              step="0.01"
+              required
+              />
+
+              <label>Gasolina (preço por litro):</label>
+              <input 
+              className="input"
+              type="number"
+              placeholder="4,90"
+              min="1"
+              step="0.01"
+              required
+              />
+
+              <input className="button" type="submit" value="Calcular"/>
+            </form>
+
+        </main>
       </div>
   )
 }
